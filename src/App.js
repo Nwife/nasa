14a8a4b2a3991325184df7b72907e215e4756d63
Amaeze Ifechukwu <ifeeamaeze@gmail.com>
2022-04-24 00:57:45 +0100

    v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+//hooks & components
+import { useFetch } from './hooks/useFetch'
+import ImageList from './components/ImageList'
+
+//styles & images
 import './App.css';
+import Planet from './assets/planets.svg'
 
 function App() {
+  const {data, error, isPending} = useFetch('https://api.nasa.gov/planetary/apod?api_key=M1aTXkZ9csLWxO7dfYmXvheSD8v9fIYs5ZtZYqX7&start_date=2022-01-21&end_date=2022-02-21')
+  console.log(data)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className="logo"><h1>NASA</h1></div>
+        <div className="spaceship"><img src={Planet} alt="planet" /></div>
       </header>
+      <section className='apod-img'>
+        {error && <p className='error'>{error}</p>}
+        {isPending && <p className='loading'>loading...</p>}
+        {data && data.map(data => (
+          <ImageList key={data.title} image={data} />
+        ))}
+      </section>
     </div>
   );
 }
---
+++ src/components/ImageList.js
@@ -0,0 +1,49 @@
+import { useState, useEffect } from 'react';
+
+//styles
+import './ImageList.css';
+
+export default function ImageList({image}) {
+  const [isLiked, setIsLiked] = useState(false);
+
+  const handleLiked = () =>{
+    setIsLiked(!isLiked)
+  }
+
+  //implementing local storage, storing the like state
+  useEffect(() => {
+      localStorage.setItem('like', JSON.stringify(isLiked))
+  }, [isLiked])
+
+  useEffect(() => {
+      const likes = JSON.parse(localStorage.getItem('like'));
+      if(likes){
+          setIsLiked(likes)
+      }
+  }, [])
+
+  return (
+    <div className="img-card">
+        <h2>{image.title}</h2>
+        <img src={image.hdurl} alt="planet or space object" />
+        <div className="img-meta">
+            <h3>{image.copyright}</h3>
+            <p>{image.date}</p>
+        </div>
+        <div className="img-explanation">
+            <p>{image.explanation}</p>
+        </div>
+        <div className="like-btn">
+            <button
+             onClick={handleLiked}
+            >Like 
+            <span>
+                <svg className={isLiked ? 'liked': ''} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M7 3C4.239 3 2 5.216 2 7.95C2 10.157 2.875 15.395 11.488 20.69C11.6423 20.7839 11.8194 20.8335 12 20.8335C12.1806 20.8335 12.3577 20.7839 12.512 20.69C21.125 15.395 22 10.157 22 7.95C22 5.216 19.761 3 17 3C14.239 3 12 6 12 6C12 6 9.761 3 7 3Z" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                </svg>
+            </span>
+            </button>
+        </div>
+    </div>
+  )
+}
